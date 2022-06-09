934f7cc9d932d4f6db60cc5a1f131f2aa1ef406a
dsneddon00 <dsneddon00@gmail.com>
2022-06-08 19:32:07 -0600

    Takes inputs and generates top words from raw html

--- HTMLWordCounter.py
+++ HTMLWordCounter.py
@@ -1,7 +1,35 @@
 import requests
+import re
+from bs4 import BeautifulSoup
 
-PAGE_URL = 'http://138.68.162.48:31994/' # change the target and the port to what suits you
+PAGE_URL = "http://" + str(input("Input host IP and port ( <HOST>:<IP> ) -> "))
 
 resp = requests.get(PAGE_URL)
-html_str = resp.content.decode()
-print(html_str)
+
+
+def getHtmlOf(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f'HTTP status code of {response.status_code} returned, but 200 was expected. Exiting...')
+        exit(1)
+    else:
+        return response.content.decode()
+
+dump = getHtmlOf(PAGE_URL)
+beautify = BeautifulSoup(dump, "html.parser")
+raw = beautify.get_text()
+total = re.findall(r'\w+', raw)
+
+wordCount = {}
+
+for word in total:
+    if(word not in wordCount):
+        wordCount[word] = 1
+    else:
+        cur = wordCount.get(word)
+        wordCount[word] = cur + 1
+
+topWords = sorted(wordCount.items(), key=lambda item: item[1], reverse=True)
+
+for i in range(10):
+    print(topWords[i][0])
