fcf365bcaf511cca108f19ed51b91cd52d9712b1
Derek Sneddon <dsneddon00@gmail.com>
2022-06-09 14:02:36 -0600

    added command line arguements

--- HTMLWordCounter.py
+++ HTMLWordCounter.py
@@ -1,8 +1,8 @@
 import requests
 import re
+import click
 from bs4 import BeautifulSoup
 
-PAGE_URL = "http://" + str(input("Input host IP and port ( <HOST>:<IP> ) -> "))
 
 
 def getHtmlOf(url):
@@ -13,10 +13,12 @@ def getHtmlOf(url):
     else:
         return response.content.decode()
 
-def countOccurrencesIn(word_list):
+def countOccurrencesIn(word_list, min_length):
     word_count = {}
 
     for word in word_list:
+        if(len(word) < min_length):
+           continue
         if word not in word_count:
             word_count[word] = 1
         else:
@@ -30,12 +32,22 @@ def getAllWordsFrom(url):
     raw_text = soup.get_text()
     return re.findall(r'\w+', raw_text)
 
-def getTopWordsFrom(url):
+def getTopWordsFrom(url, min_length):
     all_words = getAllWordsFrom(url)
-    occurrences = countOccurrencesIn(all_words)
+    occurrences = countOccurrencesIn(all_words, min_length)
     return sorted(occurrences.items(), key=lambda item: item[1], reverse=True) # lambda helps sorts the dictionary by values
 
-top_words = getTopWordsFrom(PAGE_URL)
+@click.command()
+@click.option('--url', '-u', prompt='Web URL', help='URL of webpage to extract from.')
+@click.option('--length', '-l', default=0, help='Minimum word length (default: 0, no limit).')
+@click.option('--results', '-r', default=10, help='How many results printed out to the user (default: 10, no limit).')
 
-for i in range(20):
-    print(top_words[i][0])
+
+def main(url, length, results): 
+    top_words = getTopWordsFrom(url, length)
+
+    for i in range(results):
+        print(top_words[i][0])
+
+if __name__ == '__main__':	
+    main()
