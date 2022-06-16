00ee8282f123dc623d7e40b283fc7faf7e3beb8f
Derek Sneddon <dsneddon00@gmail.com>
2022-06-16 14:28:53 -0600

    Added Zone transfer function to DNS-Zoner.py

--- DNS-Zoner.py
+++ DNS-Zoner.py
@@ -10,10 +10,30 @@ import argparse
 NS = dr.Resolver()
 
 # Target
-Domain = "inlanefreight.com" # replace with your own target
+Domain = 'inlanefreight.com' # replace with your own target
 
 # Set Name Servers
-NS.nameservers = ["ns1.inlanefreight.com", "ns2.inlanefreight.com"]
+NS.nameservers = ['ns1.inlanefreight.com', 'ns2.inlanefreight.com'] # replace with your own target's nameservers
 
 # List of found subdomains
 Subdomains = []
+
+def AXFR(domain, nameserver):
+	# try zone transfers with target's domain and nameserver
+	try:
+		axfr = dz.from_xfr(dq.xfr(nameserver, domain)) # Perform the zone transfer
+		
+		if axfr: # if the zone transfer is successful
+			print("[*] Successful Zone Transfer from {}".format(nameserver))
+		
+			# add found subdomains to global "Subdomain" list
+			for record in axfr:
+				Subdomains.append("{}.{}".format(record.to_text(), domain))
+		
+	# failure of zone transfer
+	except Exception as error:
+		print(error)
+		pass
+
+if __name__ == "__main__":
+	pass	
